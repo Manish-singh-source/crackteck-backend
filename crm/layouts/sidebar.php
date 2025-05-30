8d688779cd198328f5d4eac8f50b10dedd722a64
Roshan <support@technofra.com>
2025-05-30 14:40:18 +0530

    Some icon changed and button

--- crm/layouts/sidebar.php
+++ crm/layouts/sidebar.php
@@ -118,7 +118,7 @@
                 <li>
                     <a class='tp-link' href='delivery-man.php'>
                         <!-- <i data-feather="columns"></i> -->
-                        <img width="" height="20" src="https://img.icons8.com/?size=100&id=11151&format=png&color=000000" alt="gender-neutral-user--v1" />
+                        <img width="" height="20" src="https://img.icons8.com/external-kmg-design-detailed-outline-kmg-design/64/1A1A1A/external-delivery-man-logistics-delivery-kmg-design-detailed-outline-kmg-design-2.png" alt="external-delivery-man-logistics-delivery-kmg-design-detailed-outline-kmg-design-2"/>
                         <span class="ps-1"> Delivery Man </span>
                     </a>
                 </li>
@@ -144,7 +144,7 @@
                 </li>
                 <li>
                     <a class='tp-link' href='follow-up.php'>
-                        <img width="" height="20" src="https://img.icons8.com/ios/50/1A1A1A/graph-report.png" alt="gender-neutral-user--v1" />
+                        <img width="" height="20" src="https://img.icons8.com/ios/50/1A1A1A/communication--v1.png" alt="communication--v1"/>
                         <span class="ps-1">Follow-Up</span>
                     </a>
                 </li>
@@ -163,7 +163,7 @@
                 </li>
                 <li>
                     <a class='tp-link' href='meets.php'>
-                        <img width="" height="20" src="https://img.icons8.com/ios/50/1A1A1A/services--v1.png" alt="services--v1" />
+                        <img width="" height="20" src="https://img.icons8.com/dotty/80/1A1A1A/meeting.png" alt="meeting"/>
                         <span class="ps-1"> Meets </span>
                     </a>
                 </li>
@@ -199,7 +199,7 @@
 
                 <li>
                     <a class='tp-link' href='call-logs.php'>
-                        <img width="" height="20" src="https://img.icons8.com/ios/50/1A1A1A/phone-disconnected.png" alt="phone-disconnected" />
+                        <img width="" height="20" src="https://img.icons8.com/ios/50/1A1A1A/speech-bubble-with-dots--v1.png" alt="speech-bubble-with-dots--v1"/>
                         <span class="ps-1"> Chat Logs </span>
                     </a>
                 </li>
@@ -223,8 +223,8 @@
 
                 <li>
                     <a class='tp-link' href='in-hand-products.php'>
-                        <i data-feather="columns"></i>
-                        <span>Assign Products </span>
+                        <img width="" height="20" src="https://img.icons8.com/external-others-royyan-wijaya/24/1A1A1A/external-assign-revamp-4-others-royyan-wijaya.png" alt="external-assign-revamp-4-others-royyan-wijaya"/>
+                        <span class="ps-1">Assign Products </span>
                     </a>
                 </li>
 
