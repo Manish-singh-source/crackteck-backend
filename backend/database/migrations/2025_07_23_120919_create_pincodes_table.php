29fcdb0bcc20e3b61204d166ecb514058d2f416a
saurabh <support@technofra.com>
2025-08-29 10:43:33 +0530

    db files updated

--- backend/database/migrations/2025_07_23_120919_create_pincodes_table.php
+++ backend/database/migrations/2025_07_23_120919_create_pincodes_table.php
@@ -30,3 +30,16 @@ return new class extends Migration
         Schema::dropIfExists('pincodes');
     }
 };
+
+
+?>
+<p>
+    <b>
+        <span lang="EN-IN" style="font-size: 11.0pt; line-height: 107%; font-family: 'Calibri',sans-serif; mso-ascii-theme-font: minor-latin; mso-fareast-font-family: Calibri; mso-fareast-theme-font: minor-latin; mso-hansi-theme-font: minor-latin; mso-bidi-font-family: 'Times New Roman'; mso-bidi-theme-font: minor-bidi; mso-ansi-language: EN-IN; mso-fareast-language: EN-US; mso-bidi-language: AR-SA;">
+            Chic short-sleeve set with comfy mid-rise bottoms
+        </span>
+    </b>
+    <span lang="EN-IN" style="font-size: 11.0pt; line-height: 107%; font-family: 'Calibri',sans-serif; mso-ascii-theme-font: minor-latin; mso-fareast-font-family: Calibri; mso-fareast-theme-font: minor-latin; mso-hansi-theme-font: minor-latin; mso-bidi-font-family: 'Times New Roman'; mso-bidi-theme-font: minor-bidi; mso-ansi-language: EN-IN; mso-fareast-language: EN-US; mso-bidi-language: AR-SA;"> 
+        Soft breathable fabric | Relaxed fit | Elastic waist | All-day comfort | Full length
+    </span>
+</p>
