57453100be689d9d4bee4f34a91c1dfee18ac2c9
manish <manish@technofra.com>
2025-09-09 11:42:36 +0530

    clean code

--- warehouse/assets/js/pages/quilljs.init.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var quill = new Quill("#quill-editor", { theme: "snow" });
-var quill1 = new Quill("#quill-editor1", { theme: "snow" });
-var quill2 = new Quill("#quill-editor2", { theme: "snow" });
