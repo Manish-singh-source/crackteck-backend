08b8eccdc1a3757496bbc313c56de819b9e4d696
Roshan <support@technofra.com>
2025-09-29 12:29:26 +0530

    modification in signup page

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -14,15 +14,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
     <!-- App favicon -->
-    <link rel="shortcut icon" href="assets/images/favicon.png">
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}">
 
     <!-- App css -->
-    <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" id="app-style" />
+    <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" id="app-style" />
 
     <!-- Icons -->
-    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
 
-    <script src="assets/js/head.js"></script>
+    <script src="{{ asset('assets/js/head.js') }}"></script>
 
 
 </head>
