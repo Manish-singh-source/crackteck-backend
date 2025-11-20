543677b469e87a18dc594299135aab7b7e55b63e
saurabh <support@technofra.com>
2025-11-20 18:10:10 +0530

    profile update added

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -2,43 +2,69 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Engineer;
+use App\Models\DeliveryMan;
+use App\Models\SalesPerson;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ProfileController extends Controller
 {
-      //
-      public function index(Request $request){
+    //
+    protected function getModelByRoleId($roleId)
+    {
+        return [
+            1 => SalesPerson::class,
+            2 => Engineer::class,
+            3 => DeliveryMan::class,
+        ][$roleId] ?? null;
+    }
+
+    public function index(Request $request)
+    {
         $validated = request()->validate([
             // validation rules if any
+            'role_id' => 'required|in:1,2,3',
             'user_id' => 'required',
         ]);
 
         if (!$validated['user_id']) {
-            return response()->json(['message' => 'Engineer ID is required'], 400);
+            return response()->json(['message' => 'User ID is required'], 400);
         }
 
-        $Engineer = Engineer::where('id', $validated['user_id'])->first();
+        $model = $this->getModelByRoleId($request->role_id);
+        if (!$model) {
+            return response()->json(['success' => false, 'message' => 'Invalid role_id provided.'], 400);
+        }
 
-        return response()->json(['Engineer' => $Engineer], 200);
+        $user = $model::where('id', $validated['user_id'])->first();
+        if (!$user) {
+            return response()->json(['success' => false, 'message' => 'User not found with the provided phone number.'], 404);
+        }
+
+        return response()->json(['user' => $user], 200);
     }
 
-    public function update(Request $request) {
+    public function update(Request $request)
+    {
         $validated = request()->validate([
             // validation rules if any
+            'role_id' => 'required|in:1,2,3',
             'user_id' => 'required',
         ]);
         
-        $Engineer = Engineer::where('id', $validated['user_id'])->update($request->all());
-
-        return response()->json(['Engineer' => $Engineer], 200);
+        $model = $this->getModelByRoleId($request->role_id);
+        if (!$model) {
+            return response()->json(['success' => false, 'message' => 'Invalid role_id provided.'], 400);
+        }
+        
+        $user = $model::findOrFail($validated['user_id']);
+        $user->address = $request->address;
+        $user->save();
+        
+        if (!$user) {
+            return response()->json(['success' => false, 'message' => 'User not updated.'], 404);
+        }
+        return response()->json(['user' => $user], 200);
     }
-
-
-
-
-
-
-   
 }
