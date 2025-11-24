362c07af20935cd53c23a4c8e5e7bd61051fb4df
Roshan <support@technofra.com>
2025-11-24 15:41:55 +0530

    delivery and sales seeder file added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -36,6 +36,8 @@ class DatabaseSeeder extends Seeder
             EngineerSeeder::class,
             PlansTableSeeder::class,
             CustomerSeeder::class,
+            DeliveryMenSeeder::class,
+            SalesPeopleSeeder::class,
         ]);
       
     }
